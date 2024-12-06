b1c69722d83a9906af8e3e481a9175722c4f67be
Deniz3546 <denizyazilm@gmail.com>
2024-12-06 18:18:59 +0300

    Hesapmakinesi

---
+++ Hesap_makinesi/Hesap_makinesi/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hesap_makinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2, toplam, carpim, bolum, fark;
+            sayi1=Convert.ToInt32(textBox1.Text);
+            sayi2=Convert.ToInt32(textBox2.Text);
+            toplam = sayi1 + sayi2;
+            fark = sayi1 - sayi2;
+            carpim = sayi1 * sayi2;
+            bolum = sayi1 / sayi2;
+            label4.Text=toplam.ToString();
+            label5.Text=fark.ToString();
+            label7.Text=bolum.ToString();
+            label9.Text=carpim.ToString();
+
+        }
+    }
+}
